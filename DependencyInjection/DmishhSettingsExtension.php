f8d2976ac51d438a68e2b537adddd0ab6f14ff65
Studio M2V <contact@studio-m2v.fr>
2024-07-29 17:33:08 +0200

    preparation migration Symfony 7

--- DependencyInjection/DmishhSettingsExtension.php
+++ DependencyInjection/DmishhSettingsExtension.php
@@ -11,7 +11,8 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
 
 /**
  * This is the class that loads and manages your bundle configuration.
